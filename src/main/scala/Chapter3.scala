0d2bae31859ae840c15bc6004464b503797c19e7
Tarik Eshaq <tarikeshaq@gmail.com>
2025-08-02 12:57:52 -0700

    adds list

---
+++ src/main/scala/Chapter3.scala
@@ -0,0 +1,8 @@
+enum List[+A]:
+ case Nil
+ case Cons(head: A, tail: List[A])
+
+object List:
+  def apply[A](as: A*): List[A] =
+    if as.isEmpty then Nil
+    else Cons(as.head, apply(as.tail*)) 
