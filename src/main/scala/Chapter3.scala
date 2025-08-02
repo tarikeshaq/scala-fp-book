7757e6a584f2d92669628daaef0c5306c8e01777
Tarik Eshaq <tarikeshaq@gmail.com>
2025-08-02 13:09:05 -0700

    fmt

--- src/main/scala/Chapter3.scala
+++ src/main/scala/Chapter3.scala
@@ -1,26 +1,26 @@
 enum List[+A]:
- case Nil
- case Cons(head: A, tail: List[A])
+  case Nil
+  case Cons(head: A, tail: List[A])
 
 object List:
   def apply[A](as: A*): List[A] =
     if as.isEmpty then Nil
-    else Cons(as.head, apply(as.tail*)) 
+    else Cons(as.head, apply(as.tail*))
   def fill[A](n: Int)(a: A): List[A] =
     if n == 0 then Nil
-    else Cons(a, fill(n-1)(a))
+    else Cons(a, fill(n - 1)(a))
   def sum(ints: List[Int]): Int =
     def sum_inner(ints: List[Int], acc: Int): Int =
       ints match {
-        case Nil => acc
+        case Nil              => acc
         case Cons(head, tail) => sum_inner(tail, acc + head)
       }
     sum_inner(ints, 0)
   def product(doubles: List[Double]): Double =
     def product_inner(doubles: List[Double], acc: Double): Double =
       doubles match {
-        case Nil => acc
-        case Cons(0.0, _) => 0.0
+        case Nil              => acc
+        case Cons(0.0, _)     => 0.0
         case Cons(head, tail) => product_inner(tail, acc * head)
       }
     product_inner(doubles, 1)
