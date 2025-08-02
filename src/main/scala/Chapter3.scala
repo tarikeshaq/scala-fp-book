654417a8f0daf4a9465a36d811c1422880e151cd
Tarik Eshaq <tarikeshaq@gmail.com>
2025-08-02 13:05:24 -0700

    adds sum and product

--- src/main/scala/Chapter3.scala
+++ src/main/scala/Chapter3.scala
@@ -6,3 +6,17 @@ object List:
   def apply[A](as: A*): List[A] =
     if as.isEmpty then Nil
     else Cons(as.head, apply(as.tail*)) 
+  def sum(ints: List[Int]): Int =
+    def sum_inner(ints: List[Int], acc: Int): Int =
+      ints match {
+        case Nil => acc
+        case Cons(head, tail) => sum_inner(tail, acc + head)
+      }
+    sum_inner(ints, 0)
+  def product(doubles: List[Double]): Double =
+    def product_inner(doubles: List[Double], acc: Double): Double =
+      doubles match {
+        case Nil => acc
+        case Cons(head, tail) => product_inner(tail, acc * head)
+      }
+    product_inner(doubles, 1)
