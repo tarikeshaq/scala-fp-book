f1bf1efa638068164c0fe17edc74f4623c0b78f3
Tarik Eshaq <tarikeshaq@gmail.com>
2025-08-02 21:43:56 -0700

    fmt

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 import Option.*
 @main def hello(): Unit =
-  val res = Some(1).flatMap(a => None) 
+  val res = Some(1).flatMap(a => None)
   println(res)
   val absO: Option[Double] => Option[Double] = lift(math.abs)
   println(absO(None))
