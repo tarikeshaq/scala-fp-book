e8bb0d53cbe4545680895e91c2fd3904107064a0
Tarik Eshaq <tarikeshaq@gmail.com>
2025-10-19 02:15:36 -0700

    more chapter 9

--- src/main/scala/Chapter9.scala
+++ src/main/scala/Chapter9.scala
@@ -1,7 +1,9 @@
-import scala.util.matching.Regex
-trait Parsers[ParseError[+_], Parser[+_]]:
+import scala.util.matching.*
+
+
+trait Parsers[ParseError, Parser[+_]]:
   extension [A, E](p: Parser[A])
-    def run(input: String): Either[ParseError[E], A]
+    def run(input: String): Either[ParseError, A]
 
     infix def or(p2: => Parser[A]): Parser[A]
 
@@ -30,6 +32,12 @@ trait Parsers[ParseError[+_], Parser[+_]]:
     def slice: Parser[String]
 
     def flatMap[B](f: A => Parser[B]): Parser[B]
+
+    def ignoreWhitespace: Parser[A] =
+      ((whitespace ** p) ** whitespace).map {
+        case ((_, a), _) => a
+      }
+
 
   object Laws:
     def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
@@ -61,6 +69,80 @@ trait Parsers[ParseError[+_], Parser[+_]]:
   def char(c: Char): Parser[Char] = string(c.toString).map(_.charAt(0))
   def string(s: String): Parser[String]
   def countAs: Parser[Int] = char('a').many.slice.map(_.size)
-  def regex(r: Regex): Parser[String]
   def numLetter: Parser[String] =
     regex("\\d".r).flatMap(n => char('a').listOfN(n.toInt)).map(_.mkString)
+
+  def digit: Parser[Int] =
+    regex("\\d".r).map(_.toInt)
+
+  def stringLiteral: Parser[String] =
+    regex("\"([^\"]*)\"".r)
+
+  def whitespace: Parser[String] =
+    (char(' ') | char('\n')).many.map(_.mkString) | unit("")
+
+
+  def regex(r: Regex): Parser[String]
+
+  def maybe(c: Char): Parser[Char] =
+    char(c) | unit(c)
+
+
+enum JSON:
+  case JNull
+  case JNumber(get: Double)
+  case JString(get: String)
+  case JBool(get: Boolean)
+  case JArray(get: IndexedSeq[JSON])
+  case JObject(get: Map[String, JSON])
+
+def parseJson[Err, Parser[+_]](
+  P: Parsers[Err, Parser]
+  ): Parser[JSON] =
+    import P.*
+
+    def parseKeyValue: Parser[(String, JSON)] =
+      (stringLiteral.ignoreWhitespace ** char(':').ignoreWhitespace ** parseJson(P) ** maybe(',')).map {
+        case (((s, _), j), _) => (s, j)
+      }
+
+    def parseJsonObject: Parser[JSON] =
+      (char('{').ignoreWhitespace
+      **
+      parseKeyValue.many.map(elements => JSON.JObject(elements.toMap))
+      **
+      char('}').ignoreWhitespace).map {
+        case ((_, res), _) => res
+      }
+
+    def parseArrayElem: Parser[JSON] =
+      (parseJson(P).ignoreWhitespace ** maybe(',')).map {
+        case (j, _) => j
+      }
+
+    def parseJsonArray: Parser[JSON] =
+      (
+        char('[').ignoreWhitespace
+        **
+        parseArrayElem.many.map(elements => JSON.JArray(elements.toIndexedSeq))
+        **
+        char(']').ignoreWhitespace
+      ).map {
+        case ((_, j), _) => j
+      }
+
+    def parseNum: Parser[JSON] =
+      (regex("\\d".r) | char('.') | char('e')).ignoreWhitespace.many.map(l => JSON.JNumber(l.mkString.toDouble))
+
+    def parseNull: Parser[JSON] =
+      string("null").map(_ => JSON.JNull)
+
+    def parseString: Parser[JSON] =
+      stringLiteral.ignoreWhitespace.map(JSON.JString(_))
+
+    def parseBool: Parser[JSON] =
+      (string("true") | string("false")).ignoreWhitespace.map(_.toBoolean).map(JSON.JBool(_))
+
+
+    parseJsonObject | parseJsonArray | parseString | parseNum | parseBool | parseNull 
+
